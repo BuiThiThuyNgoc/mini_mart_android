4bfd28fc74748c0ef57ee8aa6b5cdb8eee27f9be
BuiThiThuyNgoc <19521904@gm.uit.edu.vn>
2023-05-26 14:53:00 +0700

    submit

---
+++ mini_mart_android/app/src/main/java/com/example/mini_mart_android/database/CallApi.java
@@ -0,0 +1,27 @@
+package com.example.mini_mart_android.database;
+
+import com.example.mini_mart_android.model.Product;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface CallApi {
+    Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+
+    CallApi apiProduct = new Retrofit.Builder()
+            .baseUrl("https://mini-mart-api-backup.onrender.com/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(CallApi.class);
+
+
+    @GET("api/v1/categories")
+    Call<List<Product>> call(@Query("product") String products);
+}
